20f7a78eca43721e723a539b00ba6b57027ddb23
WanggoonL <87754634+WanggoonL@users.noreply.github.com>
2021-11-29 18:09:54 +0900

    2021.11.29_useEffect

--- src/App.js
+++ src/App.js
@@ -2,30 +2,22 @@ import Button from "./Button";
 import styles from "./App.module.css";
 import { useState, useEffect } from "react";
 
-function App() {
-  const [counter, setValue] = useState(0);
-  const [keyword, setKeyword] = useState("");
-  const onClick = () => setValue((prev) => prev + 1);
-  const onChange = (event) => setKeyword(event.target.value);
-  const iRunOnlyOnce = () => {
-    console.log("i run only once");
-  };
+function Hello() {
   useEffect(() => {
-    console.log("Call the API..");
+    console.log("hi :)");
+    return () => console.log("bye :(");
   }, []);
-  useEffect(() => {
-    if (keyword !== "" && keyword.length > 5)
-      console.log("SEARCH FOR", keyword);
-  }, [keyword]);
-  useEffect(() => {
-    console.log("Check this counter");
-  }, [counter]);
+  return <h1>Hello</h1>;
+}
+
+function App() {
+  const [showing, setShowing] = useState(false);
+  const onClick = () => setShowing((prev) => !prev);
+
   return (
     <div>
-      <input value={keyword} onChange={onChange} placeholder="Search here" />
-      <h1 className={styles.title}>{counter}</h1>
-      <Button onClick={onClick} text={"Continue"} />
-      <button onClick={onClick}>click me</button>
+      {showing ? <Hello /> : null}
+      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
     </div>
   );
 }
