a37d2ca4eb5100a80c03d4041dc7385290be99f3
WanggoonL <87754634+WanggoonL@users.noreply.github.com>
2021-12-02 16:16:08 +0900

    2021.12.02_CoinTracker

--- src/App.js
+++ src/App.js
@@ -1,47 +1,32 @@
-import Button from "./Button";
-import styles from "./App.module.css";
 import { useState, useEffect } from "react";
 
 function App() {
-  const [toDo, setToDo] = useState("");
-  const [toDos, setToDos] = useState([]);
-  const onChange = (event) => setToDo(event.target.value);
-  const onSubmit = (event) => {
-    event.preventDefault();
-    if (toDo === "") {
-      return;
-    }
-    setToDos((currentArray) => [toDo, ...currentArray]);
-    setToDo("");
-  };
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  const [amount, setAmount] = useState(0);
 
-  function deleteToDo(event) {
-    const li = event.target.parentElement;
-    li.remove();
-    return;
-  } // toDos.length -1 함수를 아직 못만듦
-
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+      .then((response) => response.json())
+      .then((json) => {
+        setCoins(json);
+        setLoading(false);
+      });
+  }, []);
   return (
     <div>
-      <h1>My To Dos ({toDos.length})</h1>
-      <form onSubmit={onSubmit}>
-        <input
-          onChange={onChange}
-          value={toDo}
-          type="text"
-          placeholder="write what you do"
-        />
-        <button>Add To Do</button>
-      </form>
-      <hr />
-      <ul>
-        {toDos.map((thing, index) => (
-          <li key={index}>
-            {thing}
-            <button onClick={deleteToDo}>X</button>
-          </li>
-        ))}
-      </ul>
+      <h1>The Coins! {loading ? "(--)" : `(${coins.length})`}</h1>
+      {loading ? (
+        <strong>Loading...</strong>
+      ) : (
+        <select>
+          {coins.map((coin) => (
+            <option>
+              {coin.name} ({coin.symbol}: ${coin.quotes.USD.price})
+            </option>
+          ))}
+        </select>
+      )}
     </div>
   );
 }
